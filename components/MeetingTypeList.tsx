5ef6d39f84ea82b78e3df4c7abc3f6d0d1116e1b
Suganth <suganthchandran07@gmail.com>
2024-11-13 23:15:51 +0530

    Home Page Card Updated

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import HomeCard from "./HomeCard"
+import { useState } from "react"
+import MeetingModal from './MeetingModal';
+
+const MeetingTypeList = () => {
+
+    const router = useRouter();
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
+
+    const createMeeting = () => {
+
+    }
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+       <HomeCard
+            img="/icons/add-meeting.svg"
+            title="New Meeting"
+            desc="Start an instant meeting"
+            handleClick={() => setMeetingState('isInstantMeeting')}
+            className="bg-orange-1"
+       />
+       <HomeCard
+            img="/icons/schedule.svg"
+            title="Schedule Meeting"
+            desc="Plan your meeting"
+            handleClick={() => setMeetingState('isScheduleMeeting')}
+            className="bg-blue-1"
+       />
+       <HomeCard
+            img="/icons/recordings.svg"
+            title="View Recordings"
+            desc="Check out your recordings"
+            handleClick={() => router.push('/recordings')}
+            className="bg-purple-1"
+       />
+       <HomeCard
+            img="/icons/join-meeting.svg"
+            title="Join Meeting"
+            desc="via invitation link"
+            handleClick={() => setMeetingState('isJoiningMeeting')}
+            className="bg-yellow-1"
+       />
+
+       <MeetingModal
+          isOpen={meetingState === 'isInstantMeeting'}
+          onClose={()=> setMeetingState(undefined)}
+          title="Start an Instant Meeting"
+          className="text-center"
+          buttonText="Start Meeting"
+          handleClick={createMeeting}
+       />
+    </section>
+  )
+}
+
+export default MeetingTypeList
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,18 @@ export default {
 			},
 			sky: {
 				1: '#C9DDFF',
-			}
+				2: '#ECF0FF',
+				3: '#F5FCFF',
+			},
+			orange: {
+				1: '#FF742E',
+			},
+			purple: {
+				1: '#830EF9',
+			},
+			yellow: {
+				1: '#F9A90E',
+			},
 		},
 		backgroundImage: {
 			hero: "url('/images/hero-background.png')"
