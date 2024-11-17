a24839cab03f8480e728926c5caf7d612fde7a26
Suganth <suganthchandran07@gmail.com>
2024-11-18 02:37:10 +0530

    Call List Updated

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,17 +1,69 @@
 "use client"
 
 import { useGetCalls } from '@/hooks/useGetCalls'
-import React from 'react'
+import { CallRecording } from '@stream-io/node-sdk';
+import { Call } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import MeetingCard from './MeetingCard';
 
 const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
 
     const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
-    const 
+    const router = useRouter();
+    const [recordings, setRecordings] = useState<CallRecording[]>([]);
 
+    const getCalls = () => {
+      switch ( type ) {
+        case 'ended':
+          return endedCalls;
+        case 'recordings':
+          return recordings;
+        case 'upcoming':
+          return upcomingCalls;
+        default:
+          return [];
+      }
+    }
+
+    const getNoCallsMessage = () => {
+      switch ( type ) {
+        case 'ended':
+          return 'No Previous Calls';
+        case 'recordings':
+          return 'No Recordings';
+        case 'upcoming':
+          return 'No Upcoming calls';
+        default:
+          return [];
+      }
+    }
+
+    const calls = getCalls();
+    const noCallsMessage = getNoCallsMessage();
 
   return (
-    <div className=''>
-        
+    <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
+        {
+          calls && calls.length > 0 
+          ?
+          calls.map((meeting : Call | CallRecording)=>(
+            <MeetingCard
+              icon={}
+              title={}
+              date={}
+              isPreviousMeeting={}
+              buttonIcon1={}
+              handleClick={}
+              link={}
+              buttonText={}
+            />
+          ))
+          :
+          (
+            <h1>{noCallsMessage}</h1>
+          )
+        }
     </div>
   )
 }
